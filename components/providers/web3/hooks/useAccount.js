2bb28f1c8f9ca13e77a05ac9330a9faf05c5decd
Deep <pateldeep5724@gmail.com>
2021-12-12 17:45:17 +0530

    - admin account hash

--- components/providers/web3/hooks/useAccount.js
+++ components/providers/web3/hooks/useAccount.js
@@ -1,8 +1,12 @@
 import { useEffect, useState } from "react"
 import useSWR from "swr";
 
+const adminAddresses = {
+    "0x85eb8914bdb092332bc6c252911ed937af026eaed1013b24a585f7c41b2d5cdb": true
+}
+
 export const handler = (web3, provider) => () => {
-    const { mutate, ...rest } = useSWR(() => web3 ? "web3/accounts" : null,
+    const { data, mutate, ...rest } = useSWR(() => web3 ? "web3/accounts" : null,
         async (arg) => {
             const accs = await web3.eth.getAccounts();
             return (accs[0]);
@@ -16,6 +20,11 @@ export const handler = (web3, provider) => () => {
     }, [provider])
 
     return {
-        account: { ...rest, mutate },
+        account: {
+            data,
+            isAdmin: (data && adminAddresses[web3.utils.keccak256(data)]) ?? false,
+            mutate,
+            ...rest
+        }
     }
 }
